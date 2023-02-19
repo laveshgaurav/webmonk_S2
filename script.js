44cd29ef785c852ed7e348f5b21aa3642efec8fd
Lavesh Gaurav <39463503+laveshgaurav@users.noreply.github.com>
2023-02-19 20:25:07 +0530

    new s

---
+++ script.js
@@ -0,0 +1,65 @@
+// var myName = "First Name";
+// console.log("my name is " + myName);
+// myName = "Second Name";
+// console.log(`my name is ${myName}`);
+// myName = "Third Name";
+// console.log(`my name is ${myName}`);
+
+// var f_name = "First Name";
+// var l_name = "Last Name";
+
+// var f_name = "First Name 2";
+// var f_name = "First Name 2";
+
+// console.log("my name is " + f_name + " " + l_name);
+// console.log(`my name is ${f_name} ${l_name} `);
+
+// var x = 10;
+// var y = 20;
+// var z = 30;
+
+// var sum = x + y + z;
+
+// console.log(`The sum of ${x} + ${y} + ${z} is ${sum}`);
+
+// const MY_NAME = "First Name";
+// console.log("my name is " + MY_NAME);
+
+// let first_name = "First Name";
+// let secondName = "First Name";
+// const AGE = 20;
+
+// DATATYPES
+
+// BIGINT
+// var x = null;
+// console.log(typeof x);
+
+// var marks = [100, 120, -150, 300];
+// console.log(marks.length);
+
+let users = [
+  {
+    name: "First Name",
+    age: 20,
+    hobbies: ["cricket", "football", "basketball"],
+    address: {
+      city: "City Name",
+      state: "State Name",
+      pincode: 123456,
+    },
+  },
+  {
+    name: "Second Name",
+    full_name: "Second Name",
+    age: 40,
+    hobbies: ["badminton", "baseball", "volleyball"],
+    address: {
+      city: "City Name 2",
+      state: "State Name 2",
+      pincode: 678234,
+    },
+  },
+];
+
+console.log(users[0].name);
